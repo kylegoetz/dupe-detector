7e5fe28bbe32791339a7e1782c6bf978689a5114
Kyle Goetz <kylegoetz@gmail.com>
2020-01-12 15:20:53 -0600

    handle hash

--- src/domain/ComputeHashUseCase.kt
+++ src/domain/ComputeHashUseCase.kt
@@ -1,56 +1,68 @@
 package photo.backup.kt.domain
 
-import arrow.core.None
 import arrow.core.Option
 import arrow.core.Some
 import arrow.fx.IO
-import arrow.fx.extensions.fx
-import ch.frankel.slf4k.trace
-import org.slf4j.LoggerFactory
+import arrow.fx.extensions.io.monad.flatten
 import photo.backup.kt.SessionId
 import photo.backup.kt.data.FileEntity
 import photo.backup.kt.data.Hash
 import photo.backup.kt.data.HashEntity
 import photo.backup.kt.data.HashId
 import photo.backup.kt.data.source.IBackupRepository
-import java.util.*
-import kotlin.system.measureTimeMillis
 
-private val logger = LoggerFactory.getLogger("test")
-class ComputeHashUseCase(private val repository: IBackupRepository, val hasher: suspend (ByteArray)->Hash, val sessionId: SessionId = SessionId(UUID.randomUUID())) {
-    operator fun invoke(data: ByteArray, path: String, lastModified: Long, stage: StageType): IO<HashId> = IO.fx {
-        logger.trace { "Computing hash for $path" }
-        val (entity: Option<FileEntity>) = effect { repository.getBackup(path, stage) }
-        if(hashIsRenewable(entity, lastModified)) {
-            with(entity as Some) {
-                logger.trace { "Renewing hash for $path" }
-                !effect { repository.renewHash((entity.t.hash as Some).t, sessionId) }
-                (entity.t.hash as Some).t
+fun generateComputeHashUseCase(repository: IBackupRepository,
+                               hashFn: (ByteArray)->Hash,
+                               sessionId: SessionId): (ByteArray, String, Long, StageType)->IO<HashId> {
+
+    return { data, pathStr, lastModified, stage ->
+        IO.effect { repository.getBackup(pathStr, stage) }.map {
+            when(determineBranch(it, lastModified)) {
+                is RedoBranch -> hashSaveAndGetId(data, hashFn, repository, sessionId)
+                is RenewableBranch -> {
+                    val entity = (it as Some).t
+                    val hashId = (entity.hash as Some).t
+                    updateCase(hashId, sessionId, repository)
+                }
             }
-        } else {
-            logger.trace { "Computing hash for $path"}
-            val (hash: Hash) = effect { hasher(data) }
-            logger.trace { "Trace computed for $path"}
-            !effect { repository.upsertHash(HashEntity(hash, sessionId)) }
-//            val (result: Option<HashId>) = effect { repository.getHashId(hash) }
-//            when (result) {
-//                is Some -> {
-//                    logger.trace { "Renewing hash for $path"}
-//                    !effect { repository.renewHash(result.t, sessionId) }
-//                    result.t
-//                }
-//                is None -> {
-//                    logger.trace { "Adding hash for $path" }
-//                    val hashEntity = HashEntity(hash, sessionId)
-//                    !effect { repository.addHash(hashEntity) }
-//                }
-//            }
-        }
+        }.flatten()
     }
+}
 
-    private fun hashIsRenewable(entity: Option<FileEntity>, lastModified: Long): Boolean {
-        return entity is Some
-                && entity.t.dateModified == lastModified
-                && entity.t.hash is Some
+private fun isRenewable(entity: FileEntity, lastModified: Long): Boolean =
+    (entity.dateModified == lastModified && entity.hash is Some)
+
+private val hashSaveAndGetId: (data: ByteArray, hashFn: (ByteArray) -> Hash, repository: IBackupRepository, sessionId: SessionId) -> IO<HashId> = { data, hashFn, repository, sessionId ->
+    IO.effect { repository.upsertHash(HashEntity(hashFn(data), sessionId)) }
+}
+
+private fun updateCase(id: HashId, sessionId: SessionId, repository: IBackupRepository): IO<HashId> =
+    IO.effect { repository.renewHash(id, sessionId) }.map { id }
+
+private sealed class HashBranch {
+    sealed class MustRedo: HashBranch() {
+        object NoEntity: MustRedo()
+        object ExpiredHash: MustRedo()
+        object NoHash: MustRedo()
     }
+    object Renewable: HashBranch()
+}
+private typealias NoEntityBranch=HashBranch.MustRedo.NoEntity
+private typealias ExpiredBranch=HashBranch.MustRedo.ExpiredHash
+private typealias NoHashBranch=HashBranch.MustRedo.NoHash
+private typealias RenewableBranch=HashBranch.Renewable
+private typealias RedoBranch=HashBranch.MustRedo
+
+private fun determineBranch(maybeEntity: Option<FileEntity>, lastModified: Long): HashBranch {
+    return maybeEntity.fold({NoEntityBranch}, { entity ->
+        entity.hash.fold({
+            NoHashBranch
+        }, { // very odd error
+            val rv: HashBranch = when(isRenewable(entity, lastModified)) {
+                true -> RenewableBranch
+                false -> ExpiredBranch
+            }
+            rv
+        })
+    })
 }
---
+++ test/domain/ComputeHashUseCaseTest.kt
@@ -0,0 +1,135 @@
+package photo.backup.kt.domain
+
+import arrow.core.Either
+import arrow.core.None
+import arrow.core.Some
+import arrow.fx.IO
+import io.mockk.*
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import photo.backup.kt.SessionId
+import photo.backup.kt.data.source.IBackupRepository
+import java.util.*
+import photo.backup.kt.data.*
+import java.io.File
+
+class ComputeHashUseCaseTest {
+
+    @MockK lateinit var repo: IBackupRepository
+    private val sessionId = SessionId(UUID.randomUUID())
+    private lateinit var case: (ByteArray, String, Long, StageType)-> IO<HashId>
+    private val sourceEntity = EntityFactory.build(source, File(""), Some(HashId(UUID.randomUUID())), Media.IMAGE, sessionId)
+
+    @BeforeEach
+    fun setup() {
+        MockKAnnotations.init(this)
+        case = generateComputeHashUseCase(repo, { Hash("") }, sessionId)
+    }
+
+    @Test
+    @DisplayName("[Re-hash case] If file has changed")
+    fun reHash() {
+        coEvery { repo.getBackup(sourceEntity.absolutePath, source) } returns Some(sourceEntity)
+        coEvery { repo.upsertHash(any()) } returns HashId(UUID.randomUUID())
+
+        case(ByteArray(5), sourceEntity.absolutePath, sourceEntity.dateModified+1, source).unsafeRunSync()
+
+        coVerify { repo.upsertHash(HashEntity(Hash(""), sessionId, None)) }
+    }
+
+    @Test
+    @DisplayName("[Update Hash case] If file has not changed, updates session ID for hash entry")
+    fun unchangedFileUpdatesSessionId() {
+        coEvery { repo.getBackup(any(), source)} returns Some(sourceEntity)
+        coEvery { repo.renewHash(HashId(any()), SessionId(any()))} just runs
+
+        val hashId = case(ByteArray(5), sourceEntity.absolutePath, sourceEntity.dateModified, source).unsafeRunSync()
+
+        coVerify { repo.renewHash((sourceEntity.hash as Some).t, sessionId) }
+    }
+
+    @Test
+    @DisplayName("[Entity Has Expired case] If file has changed and hash is in DB, hash file and update hash session Id")
+    fun entityHasExpired() {
+        val file = File("")
+        val path = ""
+        coEvery { repo.getBackup(path, source) } returns Some(SourceFileEntity(
+                path,
+                None,
+                0,
+                file.lastModified(),
+                sessionId,
+                Media.IMAGE
+        ))
+        val hashId = HashId(UUID.randomUUID())
+        coEvery { repo.getHashId(Hash(any())) } returns Some(hashId)
+        coEvery { repo.upsertHash(any()) } returns hashId
+
+        case(ByteArray(5), path, file.lastModified()+1, source).unsafeRunSync()
+
+//        coVerify { repo.renewHash(hashId, sessionId) }
+        coVerify { repo.upsertHash(HashEntity(hash=Hash(""), sessionId=sessionId, id=None)) }
+    }
+
+    @Test
+    @DisplayName("[No Hash case] If file has changed and the hash is not in the DB, it upserts hash coming from hash fn")
+    fun noHashCase() {
+        val path = ""
+        coEvery { repo.getBackup(path, source) } returns Some(SourceFileEntity(
+                path,
+                None,
+                0,
+                File(path).lastModified(),
+                sessionId,
+                Media.IMAGE
+        ))
+        coEvery { repo.getHashId(Hash(any())) } returns None
+        coEvery { repo.addHash(any())} returns Either.right(HashId(UUID.randomUUID()))
+        coEvery { repo.upsertHash(any()) } returns HashId(UUID.randomUUID())
+
+        case(ByteArray(5), path, File("").lastModified()+1, source).unsafeRunSync()
+
+//        coVerify { repo.addHash(any()) }
+        coVerify { repo.upsertHash(HashEntity(Hash(""), sessionId, None)) }
+    }
+
+    @Test
+    @DisplayName("[No Entity case] If file is new, upsert hash with session ID")
+    fun noEntityCase() {
+        val path = ""
+        coEvery { repo.getBackup(path, source) } returns None
+        coEvery { repo.getHashId(Hash(any())) } returns None
+        coEvery { repo.addHash(any())} returns Either.right(HashId(UUID.randomUUID()))
+        coEvery { repo.upsertHash(any()) } returns HashId(UUID.randomUUID())
+
+        case(ByteArray(5), path, 0, source).unsafeRunSync()
+
+        coVerify { repo.upsertHash(HashEntity(Hash(""), sessionId, None)) }
+    }
+
+    @Test
+    @DisplayName("[Renew case] If file has not changed, return stored Hash and do not hash the content")
+    fun unchangedFileGetOldHash() {
+        val path = "/foo/bar"
+        val expectedHashId = HashId(UUID.randomUUID())
+        val lastModified = 123L
+        coEvery { repo.getBackup(path, source) } returns Some(SourceFileEntity(
+                path,
+                Some(expectedHashId),
+                0,
+                lastModified,
+                sessionId,
+                Media.IMAGE))
+        coEvery { repo.renewHash(expectedHashId, sessionId) } just runs
+//        val case = ComputeHashUseCase(repo, { Hash("") }, sessionId)
+
+        val hashId: HashId = case(ByteArray(5), path, lastModified, source).unsafeRunSync()
+
+        coVerify(exactly=0) { repo.getHashId(Hash(any())) }
+        assertEquals(expectedHashId, hashId)
+    }
+}
