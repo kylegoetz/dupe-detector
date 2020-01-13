63ef026e0ac7e827f858bc07f6d3d2bb84d477a4
Kyle Goetz <kylegoetz@gmail.com>
2020-01-12 23:59:27 -0600

    refactor

--- src/domain/HashByteStringUseCase.kt
+++
@@ -1,9 +0,0 @@
-package photo.backup.kt.domain
-
-import arrow.fx.IO
-import arrow.fx.extensions.fx
-import photo.backup.kt.data.Hash
-
-class HashByteStringUseCase(private val hasher: suspend (ByteArray) -> Hash) {
-    suspend operator fun invoke(bytes: ByteArray): Hash = hasher(bytes)
-}
