1889ea868cb6edfa71e5f07ff6aae257b81c5e01
Kyle Goetz <kylegoetz@gmail.com>
2020-01-16 22:34:00 -0600

    add backupEntity to base cast test

--- test/domain/BaseUseCaseTest.kt
+++ test/domain/BaseUseCaseTest.kt
@@ -5,10 +5,7 @@ import io.mockk.MockKAnnotations
 import io.mockk.impl.annotations.MockK
 import org.junit.jupiter.api.BeforeEach
 import photo.backup.kt.SessionId
-import photo.backup.kt.data.HashId
-import photo.backup.kt.data.Media
-import photo.backup.kt.data.SourceFileEntity
-import photo.backup.kt.data.SourcePhotoId
+import photo.backup.kt.data.*
 import photo.backup.kt.data.source.IBackupRepository
 import java.util.*
 
@@ -17,6 +14,7 @@ abstract class BaseUseCaseTest<T> {
     @MockK protected lateinit var repo: IBackupRepository
 
     protected lateinit var sourceEntity: SourceFileEntity
+    protected lateinit var backupEntity: GoogleFileEntity
     protected val sessionId = SessionId(UUID.randomUUID())
 
     @BeforeEach
@@ -29,6 +27,14 @@ abstract class BaseUseCaseTest<T> {
             dateModified = 0L,
             type = Media.IMAGE,
             id = SourcePhotoId(UUID.randomUUID()),
+            sessionId = SessionId(UUID.randomUUID()))
+        backupEntity = GoogleFileEntity(
+            absolutePath = "",
+            hash = Some(HashId(UUID.randomUUID())),
+            size=0L,
+            dateModified = 0L,
+            type = Media.IMAGE,
+            id = GooglePhotoId(UUID.randomUUID()),
             sessionId = SessionId(UUID.randomUUID()))
         configureSUT()
     }
