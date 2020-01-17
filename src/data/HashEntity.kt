12369ccde9660f04e7f09f195393eee606a3a000
Kyle Goetz <kylegoetz@gmail.com>
2020-01-16 22:32:25 -0600

    make hash entity have optional hashid

--- src/data/HashEntity.kt
+++ src/data/HashEntity.kt
@@ -1,5 +1,8 @@
 package photo.backup.kt.data
 
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.Some
 import org.jetbrains.exposed.dao.*
 import photo.backup.kt.SessionId
 import java.util.UUID
@@ -11,9 +14,9 @@ inline class Hash(val value: String)
 data class HashEntity(
     val hash: Hash,
     val sessionId: SessionId,
-    val id: HashId = HashId(UUID.randomUUID())
+    val id: Option<HashId> = None
 ) {
-    constructor(row: HashRow) : this(hash=Hash(row.hash), sessionId=SessionId(row.sessionId), id=HashId(row.id.value))
+    constructor(row: HashRow) : this(hash=Hash(row.hash), sessionId=SessionId(row.sessionId), id= Some(HashId(row.id.value)))
 }
 
 object HashTable : UUIDTable(name="hashes") {
